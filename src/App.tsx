324302c1f334bc60219a75d56f747cb52cc73ba3
Pablo Mayobre <pablo.a.mayobre@gmail.com>
2021-08-21 13:20:23 -0300

    Delete base app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Header } from "./components/header";
 
@@ -7,20 +5,6 @@ function App() {
   return (
     <ChakraProvider>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
         <Header />
       </div>
     </ChakraProvider>
