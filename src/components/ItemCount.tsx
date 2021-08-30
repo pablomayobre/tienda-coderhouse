066fd7516ee450a3a8ba7e7ced87e1db201bc132
Pablo Mayobre <pablo.a.mayobre@gmail.com>
2021-08-30 04:29:18 -0300

    Improve ItemCount with Chakra's better NumberInput hooks

--- src/components/ItemCount.tsx
+++ src/components/ItemCount.tsx
@@ -3,51 +3,20 @@ import {
   IconButton,
   Icon,
   Input,
-  // useNumberInput,
-  // UseNumberInputProps,
+  useNumberInput,
+  UseNumberInputProps,
 } from "@chakra-ui/react";
-import { useCallback, useEffect } from "react";
-import { useState } from "react";
 import { MdAdd, MdRemove } from "react-icons/md";
-import { useCallbackProp } from "../hooks/useCallbackProp";
 
-type ItemCountProps = {
-  initialValue?: number;
-  onChange?: (value: number) => void;
-  max?: number;
-  min?: number;
-  step?: number;
-};
-
-export const ItemCount = ({
-  onChange,
-  initialValue,
-  max = Infinity,
-  min = -Infinity,
-  step = 1,
-}: ItemCountProps) => {
-  const [count, setCount] = useState(initialValue ?? 0);
-
-  const handler = useCallbackProp(onChange);
-
-  const increment = useCallback(
-    () => setCount((start) => Math.min(start + step, max)),
-    [setCount, max, step]
-  );
-  const decrement = useCallback(
-    () => setCount((start) => Math.max(start - step, min)),
-    [setCount, min, step]
-  );
-
-  useEffect(() => {
-    handler(count);
-  }, [handler, count]);
+export const ItemCount = (props: UseNumberInputProps) => {
+  const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
+    useNumberInput({ ...props, defaultValue: props.defaultValue ?? 0, allowMouseWheel: true });
 
   return (
     <Flex maxWidth={64}>
       <IconButton
+        {...getIncrementButtonProps()}
         icon={<Icon as={MdAdd} size={5} />}
-        onClick={increment}
         aria-label="Incrementar"
         borderTopRightRadius={0}
         borderBottomRightRadius={0}
@@ -55,12 +24,8 @@ export const ItemCount = ({
         variant="outline"
       />
       <Input
+        {...getInputProps()}
         borderRadius={0}
-        type="number"
-        value={count}
-        onChange={(e) =>
-          setCount(Math.min(Math.max(Number(e.target.value), min), max))
-        }
         colorScheme="purple"
         borderColor="purple.500"
         borderLeft={0}
@@ -68,8 +33,8 @@ export const ItemCount = ({
         textAlign="center"
       />
       <IconButton
+        {...getDecrementButtonProps()}
         icon={<Icon as={MdRemove} size={5} />}
-        onClick={decrement}
         aria-label="Decrementar"
         borderTopLeftRadius={0}
         borderBottomLeftRadius={0}
@@ -79,40 +44,3 @@ export const ItemCount = ({
     </Flex>
   );
 };
-
-// export const BetterItemCount = (props: UseNumberInputProps) => {
-//   const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
-//     useNumberInput({ ...props, allowMouseWheel: true });
-
-//   return (
-//     <Flex maxWidth={64}>
-//       <IconButton
-//         {...getIncrementButtonProps()}
-//         icon={<Icon as={MdAdd} size={5} />}
-//         aria-label="Incrementar"
-//         borderTopRightRadius={0}
-//         borderBottomRightRadius={0}
-//         colorScheme="purple"
-//         variant="outline"
-//       />
-//       <Input
-//         {...getInputProps()}
-//         borderRadius={0}
-//         colorScheme="purple"
-//         borderColor="purple.500"
-//         borderLeft={0}
-//         borderRight={0}
-//         textAlign="center"
-//       />
-//       <IconButton
-//         {...getDecrementButtonProps()}
-//         icon={<Icon as={MdRemove} size={5} />}
-//         aria-label="Decrementar"
-//         borderTopLeftRadius={0}
-//         borderBottomLeftRadius={0}
-//         colorScheme="purple"
-//         variant="outline"
-//       />
-//     </Flex>
-//   );
-// };
