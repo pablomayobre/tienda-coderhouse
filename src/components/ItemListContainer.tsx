0463df8c9375f06cc61f57110939113d48aad466
Pablo Mayobre <pablo.a.mayobre@gmail.com>
2021-08-30 04:12:42 -0300

    Remove ItemCount and prepare ItemListContainer

--- src/components/ItemListContainer.tsx
+++ src/components/ItemListContainer.tsx
@@ -1,7 +1,7 @@
 import { Box, Heading } from "@chakra-ui/react"
 
-export const ItemListContainer = ({greeting}: {greeting?: string}) => {
-  return <Box>
-    <Heading>{greeting}</Heading>
+export const ItemListContainer = () => {
+  return <Box paddingBottom={4}>
+    <Heading as="h2" textAlign="center">Productos</Heading>
   </Box>
 }
