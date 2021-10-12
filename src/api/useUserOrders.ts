29e9738bd2dd87492c0782c639fe351625f74a24
Pablo Mayobre <pablo.a.mayobre@gmail.com>
2021-10-12 12:25:26 -0300

    Sort UserOrders by date

--- src/api/useUserOrders.ts
+++ src/api/useUserOrders.ts
@@ -1,4 +1,4 @@
-import { collection, query, where } from "firebase/firestore";
+import { collection, orderBy, query, where } from "firebase/firestore";
 import { useFirestore, useFirestoreCollectionData, useUser } from "reactfire";
 import { FullOrder } from "./useSaveOrder";
 
@@ -9,7 +9,7 @@ export const useUserOrders = () => {
   const itemsCollection = collection(firestore, "orders");
 
   const { data } = useFirestoreCollectionData(
-    query(itemsCollection, user ? where("user", "==", user.uid) : where("id", "==", null)),
+    query(itemsCollection, user ? where("user", "==", user.uid) : where("id", "==", null), orderBy("date", "desc")),
     {idField: "id"}
   );
 
